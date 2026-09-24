581004a9bedf36868680e22c2116068bbe0e36a6
marmocc <contact@marmo.cc>
2026-09-24 11:39:44 +0200

    Performance optimizations for Color.Density

--- Etch/Graphics/Color.cs
+++ Etch/Graphics/Color.cs
@@ -3,14 +3,22 @@ using System.Runtime.InteropServices;
 
 namespace Etch.Graphics;
 
-[StructLayout(LayoutKind.Sequential)]
+[StructLayout(LayoutKind.Sequential, Pack = 1)]
 public readonly struct Color(byte r, byte g, byte b, byte a = 255) : IEquatable<Color>
 {
     public readonly byte R = r, G = g, B = b, A = a; // Fits into a uint
 
-    public float Luminance => (0.2126f * R + 0.7152f * G + 0.0722f * B) / 255f;
-    public static readonly byte[] Ramp = [32, 46, 58, 45, 61, 43, 42, 35, 37, 64]; // " .:-=+*#%@"
-    public byte Density => Ramp[Math.Clamp((int)(Luminance * Ramp.Length), 0, Ramp.Length - 1)];
+    public readonly byte Density
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        get
+        {
+            ReadOnlySpan<byte> ramp = " .:-=+*#%@"u8;
+            int index = (8 * R + 26 * G + 3 * B) >> 10;
+            if ((uint)index >= 10) index = 9;
+            return ramp[index];
+        }
+    }
 
     public static Color Transparent => new(0, 0, 0, 0);
     public static Color White => new(255, 255, 255, 255);
@@ -24,6 +32,7 @@ public readonly struct Color(byte r, byte g, byte b, byte a = 255) : IEquatable<
     public Color WithBlue(byte blue) => new(R, G, blue, A);
     public Color WithAlpha(byte alpha) => new(R, G, B, alpha);
 
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public bool Equals(Color other) =>
         Unsafe.As<Color, uint>(ref Unsafe.AsRef(in this)) ==
         Unsafe.As<Color, uint>(ref Unsafe.AsRef(in other));
@@ -31,21 +40,23 @@ public readonly struct Color(byte r, byte g, byte b, byte a = 255) : IEquatable<
         obj is Color color && Equals(color);
     public static bool operator ==(Color left, Color right) => left.Equals(right);
     public static bool operator !=(Color left, Color right) => !(left == right);
-    public override int GetHashCode() => HashCode.Combine(R, G, B, A);
+    public override int GetHashCode() => (int)Unsafe.As<Color, uint>(ref Unsafe.AsRef(in this));
     public override string ToString() => $"({R},{G},{B},{A})";
 
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static Color Blend(Color destination, Color source)
     {
         if (source.A == 255) return source;
         if (source.A == 0) return destination;
 
-        float alphaFactor = source.A / 255f;
-        float invAlphaFactor = 1f - alphaFactor;
+        int sA = source.A;
+        int dA = 255 - sA;
 
-        byte r = (byte)(source.R * alphaFactor + destination.R * invAlphaFactor);
-        byte g = (byte)(source.G * alphaFactor + destination.G * invAlphaFactor);
-        byte b = (byte)(source.B * alphaFactor + destination.B * invAlphaFactor);
+        // High-speed integer approximation of standard alpha blending
+        byte r = (byte)((source.R * sA + destination.R * dA) / 255);
+        byte g = (byte)((source.G * sA + destination.G * dA) / 255);
+        byte b = (byte)((source.B * sA + destination.B * dA) / 255);
 
-        return new(r, g, b, 255);
+        return new Color(r, g, b, 255);
     }
 }
