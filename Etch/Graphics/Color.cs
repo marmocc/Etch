791892048997cb82b100557b12d764d7120fea05
marmocc <contact@marmo.cc>
2026-09-25 16:55:41 +0200

    Expanded Color.Glyph ramp to a 70 characters one based on Paul Bourke's, but slightly modified to fix some bending.

--- Etch/Graphics/Color.cs
+++ Etch/Graphics/Color.cs
@@ -19,8 +19,8 @@ public readonly struct Color(byte r, byte g, byte b, byte a = 255) : IEquatable<
         get
         {
             int luminance = (306 * R + 601 * G + 117 * B) >> 10;
-            int index = (luminance * 32) >> 8;
-            ReadOnlySpan<byte> ramp = " .'-!*+|r=vL7%1JCwX$Zm#U8&0OWQ@B"u8;
+            int index = (luminance * 70) >> 8;
+            ReadOnlySpan<byte> ramp = " .'`^\",_-~:;!><+il?I][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$"u8;
             return ramp[index];
         }
     }
